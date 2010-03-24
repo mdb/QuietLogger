a50629aab19fdcfb4e0f0b89485bbab24446f0f8
Mike Ball <mikedball@gmail.com>
2010-03-24 11:02:25 -0400

    Created div#respond and nav.post-navigation styles

--- footer.php
+++ footer.php
@@ -1,19 +1,5 @@
       <footer id="site-footer">
-        <?php get_search_form(); ?>
-				<div class="content-section">
-					<ul>
-						<li><a href="">August 2010</a></li>
-						<li><a href="">September 2010</a></li>
-						<li><a href="">October 2010</a></li>
-					</ul>	
-				</div>
-				<div class="content-section">
-					
-				</div>
-				<div class="content-section">
-					
-				</div>								
-				
+        <?php get_search_form(); ?>	
       </footer>
     </div>
   </body>
