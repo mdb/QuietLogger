a78c6c6363c82fe9cc310bddb12b6b39fd0407d8
Mike Ball <mikedball@gmail.com>
2010-03-26 14:27:17 -0400

    Tweaked masthead h2 and added html5shiv for IE

--- header.php
+++ header.php
@@ -12,6 +12,9 @@
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
   <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/main.js"></script>  
+	<!--[if IE<9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
   <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
   <link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
   <link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="<?php bloginfo('atom_url'); ?>" />
